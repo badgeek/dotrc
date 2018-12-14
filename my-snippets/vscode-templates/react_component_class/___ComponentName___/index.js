d2d83f7449e1d872688a9a522d0539aa4adf8830
Budi Prakosa <iyok@deadmediafm.org>
2018-12-14 12:29:56 +0700

    goodies

---
+++ my-snippets/vscode-templates/react_component_class/___ComponentName___/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ___ComponentName___ extends React.Component {
+
+    constructor (props) {
+      super(props)
+      this.state = { }
+    }
+    
+    render() {
+      return <h1>Hello, {this.props.name}</h1>;
+    }
+  }
+
+export default ___ComponentName___;
